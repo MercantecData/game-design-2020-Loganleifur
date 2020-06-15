addb01d20b525583a39278cb109b47ad155acf1e
LoganLR <lilli_boy@hotmail.com>
2020-06-15 15:01:44 +0200

    Added a shooting mechanic, collision and the camera follows the player.
    :)

---
+++ An A-MAZE-ing Game/Assets/Camerafollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerafollow : MonoBehaviour
+{
+    public Transform Obama;
+
+     void FixedUpdate ()
+    {
+        transform.position = new Vector3(Obama.position.x, Obama.position.y, transform.position.z);
+    }
+    
+    
+    
+}
---
+++ An A-MAZE-ing Game/Assets/Movement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed = 10;
+
+
+    public GameObject bulletright, bulletleft;
+    Vector2 bulletPos;
+    public float fireRate = 0.5f;
+    float nextFire = 0.5f;
+
+
+
+
+    private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalinput = Input.GetAxis("Horizontal");
+        var verticalinput = Input.GetAxis("Vertical");
+
+        Vector2 position = transform.position;
+        position.x += speed * Time.deltaTime * horizontalinput;
+        position.y += speed * Time.deltaTime * verticalinput;
+        rigidbody.MovePosition(position);
+
+
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            fire();
+        }
+
+
+            void fire()
+            {
+                bulletPos = transform.position;
+                bulletPos += new Vector2 (+1.2f, 0.27f);
+                Instantiate(bulletright, bulletPos, Quaternion.identity);
+            }
+    }
+        
+
+}
+
