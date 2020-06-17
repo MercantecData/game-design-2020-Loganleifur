d82f598c64a4edd2d89627268d3461381f064871
LoganLR <lilli_boy@hotmail.com>
2020-06-17 14:31:09 +0200

    Map demo complete
    ye yeet add rooms and stuff

---
+++ An A-MAZE-ing Game/Assets/gamecontrollerscript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gamecontrollerscript : MonoBehaviour
+{
+
+    public Text ui_text;
+    // Start is called before the first frame update
+
+    public static gamecontrollerscript instance;
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    
+}
