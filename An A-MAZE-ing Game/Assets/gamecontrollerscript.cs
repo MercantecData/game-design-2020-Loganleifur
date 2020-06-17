2dbf359ebd4674b473849d11137a7343edb679fc
LoganLR <lilli_boy@hotmail.com>
2020-06-17 14:54:07 +0200

    Added the ability to commit murder :)

--- An A-MAZE-ing Game/Assets/gamecontrollerscript.cs
+++ An A-MAZE-ing Game/Assets/gamecontrollerscript.cs
@@ -7,6 +7,9 @@ public class gamecontrollerscript : MonoBehaviour
 {
 
     public Text ui_text;
+
+    //ui_text.text = "HP: " + hp thing
+    //public void OnTriggerEnter2d() { GameController.hp}
     // Start is called before the first frame update
 
     public static gamecontrollerscript instance;
