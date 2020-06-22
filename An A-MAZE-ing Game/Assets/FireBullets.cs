02ada91efbce1c99c623b28845779724f2b334e3
Logan <Lilli_boy@hotmail.com>
2020-06-22 13:36:00 +0200

    Fix

--- An A-MAZE-ing Game/Assets/FireBullets.cs
+++ An A-MAZE-ing Game/Assets/FireBullets.cs
@@ -20,7 +20,7 @@ public class FireBullets : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Shoot", 0f, 0.1f);
+        InvokeRepeating("Shoot", 0f, 2f);
 
     }
 
