aa20a3e242a8f17c6173a7b84725910919941648
Logan <Lilli_boy@hotmail.com>
2020-06-22 11:16:02 +0200

    Stage 2 Bullet Hell implemented
    tihi

--- An A-MAZE-ing Game/Assets/FireBullets.cs
+++ An A-MAZE-ing Game/Assets/FireBullets.cs
@@ -6,8 +6,8 @@ public class FireBullets : MonoBehaviour
 {
 
     public bool secondPhase = true;
-    
 
+    private float angle = 0f;
 
     [SerializeField]
     private int bulletsAmount = 10;
@@ -20,12 +20,25 @@ public class FireBullets : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Fire", 0f, 2f);
-        
+        InvokeRepeating("Shoot", 0f, 0.1f);
+
     }
 
     // Update is called once per frame
-    
+
+
+    private void Shoot()
+    {
+        if(secondPhase)
+        {
+            Fire2();
+        }
+        else
+        {
+            Fire();
+        }
+
+    }
     private void Fire()
     {
         SoundManager.PlaySound("BossFire");
@@ -50,6 +63,36 @@ public class FireBullets : MonoBehaviour
             
             
         }
+    }
+
+    private void Fire2()
+    {
+
+        
+        
+        
+        for (int i = 0; i <= 1; i++)
+        {
+            float bulDirX = transform.position.x + Mathf.Sin(((angle + 180f * i) * Mathf.PI) / 180f);
+            float bulDirY = transform.position.y + Mathf.Cos(((angle + 180f * i) * Mathf.PI) / 180f);
+
+            Vector3 bulMoveVector = new Vector3(bulDirX, bulDirY, 0f);
+            Vector2 bulDir = (bulMoveVector - transform.position).normalized;
+
+            GameObject bul = BulletPool.bulletPoolInstance.GetBullet();
+            bul.transform.position = transform.position;
+            bul.transform.rotation = transform.rotation;
+            bul.SetActive(true);
+            bul.GetComponent<Bullet>().SetMoveDirection(bulDir);
+        }
+
+        angle += 15f;
+
+        if (angle >= 360f)
+        {
+            angle = 0f;
+        }
+
     }
 
     
